60031e830b150a50f85195f43b25612814dcb576
Victoria Ojeda <Vickiojeda05@gmail.com>
2025-11-13 15:01:11 -0300

    la clase persona incluye el atributo país

--- Persona.java
+++ Persona.java
@@ -1,12 +1,23 @@
+package com.taller.proyecto.logica;
+
 public class Persona{
     private String dni;
     private String nombre;
     private String apellido;
+    private Pais pais;
 
-    public Persona(String dni, String nombre, String apellido){
+    public Persona(){
+        dni = "";
+        nombre = "";
+        apellido = "";
+        pais = null;
+    }
+
+    public Persona(String dni, String nombre, String apellido, Pais pais){
         this.dni = dni;
         this.nombre = nombre;
-        this.apellido = apellido;        
+        this.apellido = apellido;  
+        this.pais = pais;      
     }
 
     public String getDni(){
@@ -21,6 +32,10 @@ public class Persona{
         return this.apellido;
     }
 
+    public Pais getPais(){
+        return this.pais;
+    }
+
     public void setDni(String dni){
         this.dni = dni;
     }
@@ -31,5 +46,9 @@ public class Persona{
 
     public void setApellido(String apellido){
         this.apellido = apellido;
+    }
+
+    public void setPais(Pais pais){
+        this.pais = pais;
     }
 }
