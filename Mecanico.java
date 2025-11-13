c7f5b0416be43f65ed0906203d3bf9fd9ccf5136
Victoria Ojeda <Vickiojeda05@gmail.com>
2025-11-13 15:00:34 -0300

    Refactor Mecanico class with additional constructors and methods

--- Mecanico.java
+++ Mecanico.java
@@ -1,11 +1,28 @@
+package com.taller.proyecto.logica;
+
+import java.util.*;
+
 public class Mecanico extends Persona{
     private Especialidad especialidad;
     private int añosExperiencia;
-    
-    public Mecanico(String dni, String nombre, String apellido, Especialidad especialidad, int añosExperiencia){
-        super(dni, nombre, apellido);
+    private List <Escuderia> escuderias;
+
+    public Mecanico(){
+        super("", "", "", null);
+        this.escuderias = new ArrayList<>();
+    }
+
+    public Mecanico(String dni, String nombre, String apellido, Pais pais, Especialidad especialidad, int añosExperiencia){
+        super(dni, nombre, apellido, pais);
         this.especialidad = especialidad;
         this.añosExperiencia = añosExperiencia;
+    }
+    
+    public Mecanico(String dni, String nombre, String apellido, Especialidad especialidad, int añosExperiencia, Pais pais, List <Escuderia> escuderias){
+        super(dni, nombre, apellido, pais);
+        this.especialidad = especialidad;
+        this.añosExperiencia = añosExperiencia;
+        this.escuderias = escuderias;
     }
 
     public Especialidad getEspecialidad(){
@@ -16,11 +33,19 @@ public class Mecanico extends Persona{
         return this.añosExperiencia;
     }
 
+    public List <Escuderia> getEscuderias(){
+        return this.escuderias;
+    }
+
     public void setEspecialidad(Especialidad especialidad){
         this.especialidad = especialidad;
     }
 
     public void setAñosExperiencia(int añosExperiencia){
         this.añosExperiencia = añosExperiencia;
+    }
+
+    public void agregarEscuderia(Escuderia e){
+        this.escuderias.add(e);
     }
 }
